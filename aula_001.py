1b2890b349eda1d9c8e83d6fef898aad81f0a8c7
LorenzoLopes <lorenzolopes.dev@gmail.com>
2024-04-19 16:06:33 -0300

    Comentarios e DocStrings

---
+++ aula_001.py
@@ -0,0 +1,10 @@
+print("Hello World")
+# Comentario;
+"""
+Isso aqui é uma DocString!!
+isso é lido pelo interpretador do codigo mas nao é executado
+"""
+'''
+DocString com aspas Simples!
+Linha de ex!
+'''
